1bf974544bc71a283ac915394ed1718fed00b063
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-15 20:33:51 -0300

    update

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose")
 const nodemon = require("nodemon")
 const Contato = require("./model/Contato")
 const app = express()
+const env = require('dotenv').config()
+const user = process.env.USER
+const password = process.env.PASSWORD
 
 app.use(
     express.urlencoded({
@@ -48,11 +51,73 @@ app.post(`/contato`, async (req, res)=>{
     }
 })
 
+app.get('/contato', async (req, res)=>{
+    try {
+        const pessoa = await Contato.find()
+        res.status(200).json(pessoa)
+    }
+    catch(error) {
+        res.status(500).json({error:error})
+    }
+})
+
+app.get('/contato/:id', async (req, res)=>{
+    const id = req.params.id
+
+    try {
+        const pessoa = await Contato.findOne({_id:id})
+        if(!pessoa){
+            res.status(422).json({error: 'Este contato não existe!'})
+        }
+        res.status(200).json(pessoa)        
+    }
+    catch(error) {
+        res.status(500).json({error:error})
+    }
+})
+
+app.delete('/contato/:id', async (req, res)=>{
+    const id = req.params.id
+    const pessoa = await Contato.findOne({_id:id})
+    if(!pessoa){
+        res.status(422).json({error: 'Este contato não existe!'})
+        return
+    }
+    
+    try {
+        await Contato.deleteOne({_id: id})
+        res.status(200).json({msg: "Contato removido com sucesso!"})        
+    }
+    catch(error) {
+        res.status(500).json({error:error})
+    }
+})
+
+app.patch(`/contato/:id`, async (req, res)=>{
+    const id = req.params.id 
+
+    const {nome, email, telefone, area, linkedin, idade} = req.body
+
+    const pessoa = {
+        nome, email, telefone, area, linkedin, idade
+    }
+
+    try{
+        const atualizarContato = await Contato.updateOne({_id:id}, pessoa)
+        //200 ok
+        res.status(200).json(pessoa)
+    }
+    catch(error){
+        res.status(500).json({error:error})
+    }
+})
+
+
 app.listen(3001)
 
 console.log(`O meu app do campinho está no ar!`)
 
-mongoose.connect('mongodb+srv://CarolinaDiniz:Webang111watercolor@project.kcg7wqs.mongodb.net/agendaCampinho?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://${user}:${password}@project.kcg7wqs.mongodb.net/agendaCampinho?retryWrites=true&w=majority`)
     .then(()=>{
         console.log("Banco de dados foi conectado com sucesso!")
     })
